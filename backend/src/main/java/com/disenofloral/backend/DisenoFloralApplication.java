a8ce8bcaf79da82951a205632b7aa960c0537900
Borja <borjajaviermartincedres@alumno.ieselrincon.es>
2025-01-14 23:01:41 +0000

    Initial commit

---
+++ backend/src/main/java/com/disenofloral/backend/DisenoFloralApplication.java
@@ -0,0 +1,13 @@
+package com.disenofloral.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisenoFloralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisenoFloralApplication.class, args);
+	}
+
+}
